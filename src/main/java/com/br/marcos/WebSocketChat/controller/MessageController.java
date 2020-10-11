82b9ecba1250f05cfcae8411a2e10db0d4cbc270
Marcos Vinícius <marcos.viniciusk8@hotmail.com>
2020-10-11 19:11:28 -0300

    Deployment project

---
+++ src/main/java/com/br/marcos/WebSocketChat/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.br.marcos.WebSocketChat.controller;
+
+import com.br.marcos.WebSocketChat.model.MessageModel;
+import com.br.marcos.WebSocketChat.storage.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, MessageModel message) {
+
+        System.out.println("handeling send message " + message + " to: " + to);
+        boolean isExists = UserStorage.getInstance().getUsers().contains(to);
+        if (isExists) {
+            simpMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
+        }
+    }
+}
